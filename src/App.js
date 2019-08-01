944444e496945c03a3171aed134aa6b63a6218a5
willymello <willy.mello@cantinaconsulting.com>
2019-08-01 14:15:10 -0400

    rotation achieved

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import logo from "./logo.svg";
+
 import "aframe";
 import { Entity, Scene } from "aframe-react";
 import "aframe-particle-system-component";
 import "./App.css";
 
 function App() {
+  const gr = 8;
+  const cp = Math.sqrt(Math.pow(gr, 2) / 2);
+  const tr = 0.01;
   return (
     <Scene
       environment={{
         preset: "starry",
         seed: 2,
         lightPosition: { x: 0.0, y: 0.03, z: -0.5 },
-        fog: 0.8,
+        fog: 0,
         ground: "canyon",
         groundYScale: 6.31,
         groundTexture: "walkernoise",
@@ -22,11 +25,74 @@ function App() {
       }}
     >
       <Entity
-        geometry={{ primitive: "box" }}
-        material={{ color: "red" }}
-        position={{ x: 0, y: 0, z: -5 }}
+        geometry={{ primitive: "sphere", radius: gr }}
+        material={{ color: "lightblue", opacity: 0 }}
+        position={{ x: 0, y: 0, z: -gr }}
+        animation={{
+          property: "rotation",
+          to: "360 0 0",
+          loop: true,
+          dur: 10000
+        }}
+        rotation={{ x: 0, y: 0, z: 0 }}
       >
+        //green block
         <Entity
+          geometry={{ primitive: "box", radius: gr / 8 }}
+          material={{ color: "green", opacity: 1 }}
+          // position={{ x: cp, y: cp, z: -gr }}
+          position={{ x: 0, y: 0, z: -gr }}
+          animation={{
+            property: "rotation",
+            dur: 12000,
+            repeat: "indefinite",
+            to: "0 360 0",
+            easing: "linear",
+            loop: true
+          }}
+          rotation={{ x: 0, y: 0, z: 0 }}
+        />
+        //red block
+        <Entity
+          geometry={{ primitive: "box", radius: gr / 8 }}
+          material={{ color: "red", opacity: 1 }}
+          // position={{ x: cp, y: cp, z: -gr }}
+          position={{ x: 0, y: 0, z: gr }}
+          animation={{
+            property: "rotation",
+            dur: 12000,
+            repeat: "indefinite",
+            to: "360 360 360",
+            easing: "linear",
+            loop: true
+          }}
+          rotation={{ x: 0, y: 0, z: 0 }}
+        />
+      </Entity>
+      <Entity
+        geometry={{ primitive: "sphere", radius: gr / 4 }}
+        material={{ color: "blue", opacity: 1 }}
+        position={{ x: -gr / 4, y: 0, z: -gr }}
+      />
+      <Entity
+        geometry={{ primitive: "sphere", radius: gr / 4 }}
+        material={{ color: "pink", opacity: 1 }}
+        position={{ x: gr / 4, y: 0, z: -gr }}
+      />
+
+      <Entity
+        geometry={{ primitive: "torus", radius: tr + gr, radiusTubular: tr }}
+        material={{ color: "red", opacity: 1 }}
+        position={{ x: 0, y: 0, z: -gr }}
+      />
+      {/* <a-box
+        rotation="0 0 0"
+        animation="property: rotation; to: 0 360 0; loop: true; dur: 10000"
+      >
+        <a-box position="5 0 0" />
+      </a-box> */}
+      <Entity camera position={{ x: 0, y: cp, 0: gr }} />
+      {/* <Entity
           primitive="a-animation"
           attribute="color"
           to="green"
@@ -34,12 +100,13 @@ function App() {
           dur="2000"
           repeat="indefinite"
           direction="alternate"
-        />
-      </Entity>
-      <Entity particle-system={{ preset: "snow" }} />
+        /> */}
+
+      {/* <Entity particle-system={{ preset: "snow" }} /> */}
       <Entity light={{ type: "point" }} />
-      <Entity gltf-model={{ src: "virtualcity.gltf" }} />
+      {/* <Entity gltf-model={{ src: "virtualcity.gltf" }} /> */}
       <Entity text={{ value: "Hello, WebVR!" }} />
+      <a-sky src={require("./img/space.png")} />
     </Scene>
     // <a-scene>
     //   <a-entity hello-world="event: anEvent; message: Howdy there" />

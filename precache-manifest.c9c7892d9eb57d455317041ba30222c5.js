40031a12c97cb36ca51ba4c731192f2efec5b050
willymello <willy.mello@cantinaconsulting.com>
2019-08-02 15:26:04 -0400

    Updates

---
+++ precache-manifest.c9c7892d9eb57d455317041ba30222c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5968ca5999c4caef38aaa0207ee150",
+    "url": "/familytree/index.html"
+  },
+  {
+    "revision": "67cfd14612b09d3b32f0",
+    "url": "/familytree/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a506cfdcf51a0b7bf272",
+    "url": "/familytree/static/js/2.8394abbf.chunk.js"
+  },
+  {
+    "revision": "67cfd14612b09d3b32f0",
+    "url": "/familytree/static/js/main.daa6114a.chunk.js"
+  },
+  {
+    "revision": "6b09ebbb20c72d0c5ed0",
+    "url": "/familytree/static/js/runtime~main.2695dad8.js"
+  },
+  {
+    "revision": "588e59c3578a10a366457df3ade7941f",
+    "url": "/familytree/static/media/space.588e59c3.png"
+  }
+]);
